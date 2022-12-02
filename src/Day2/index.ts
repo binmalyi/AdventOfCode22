6cd252396345d402f211996ee62294be6f8bc0b4
Malyi <70099365+binmalyi@users.noreply.github.com>
2022-12-02 18:22:57 +0300

    Update index.ts

--- src/Day2/index.ts
+++ src/Day2/index.ts
@@ -1,6 +1,7 @@
-const input = (await import('fs')).readFileSync('input.txt', {encoding: 'utf-8'}).replace(/\s/g, '')) // Input from the elves :)
+const input = (await import('fs')).readFileSync('input.txt', {encoding: 'utf-8'}).replace(/\s/g, '')); // Input from the elves :)
 
-function Day1Pt1(strategy: string){
+/* --------------------------------------------- [START OF PART 1] --------------------------------------------- */
+function decrypt(elfText: string){
     const overall_score: number[] = [];
     const rps = {
         A: 1, // Rock
@@ -15,8 +16,9 @@ function Day1Pt1(strategy: string){
     opp - Opponent
     user - Me ;)
     */
-    for (const [opp, user] of (strategy.split(/(\w{2})/g).filter(str => str !== '').map(values => values.split(/(\w)/g).filter(str => str !== '')) as unknown) as `${keyof typeof rps}`[][]) overall_score.push(rps[user] + rps[opp] === rps[user] ? 3 : (rps[opp] === 2 || rps[user] === 2 ? (rps[opp] > rps[user] ? 0 : 6) : (rps[opp] > rps[user] ? 6 : 0)));
+    for (const [opp, user] of (elfText.split(/(\w{2})/g).filter(str => str !== '').map(values => values.split(/(\w)/g).filter(str => str !== '')) as unknown) as `${keyof typeof rps}`[][]) overall_score.push(rps[user] + rps[opp] === rps[user] ? 3 : (rps[opp] === 2 || rps[user] === 2 ? (rps[opp] > rps[user] ? 0 : 6) : (rps[opp] > rps[user] ? 6 : 0)));
     return overall_score.reduce((acc, value) => acc + value); // Add the values (numbers) in array to get the overall total score
 };
 
-// console.log(Day1Pt1(input))
+console.log(decrypt(input));
+/* --------------------------------------------- [END OF PART 1] --------------------------------------------- */
