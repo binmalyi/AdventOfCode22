0b20c26877c94bb3a39056d13d14e43e8c0c8ada
Malyi <70099365+binmalyi@users.noreply.github.com>
2022-12-02 16:29:44 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,20 @@ const input = (await import('fs')).readFileSync('input.txt', {encoding: 'utf-8'}
 function Day1Pt1(strategy: string){
     const overall_score: number[] = [];
     const rps = {
-        A: 1,
-        B: 2,
-        C: 3,
-        X: 1,
-        Y: 2,
-        Z: 3
-    } as const;
+        A: 1, // Rock
+        B: 2, // Paper
+        C: 3, // Scissors
+        X: 1, // Rock
+        Y: 2, // Paper
+        Z: 3 // Scissors
+    } as const; // A - C are opponent's values && X - Z are user's values
     
+    /*
+    opp - Opponent
+    user - Me ;)
+    */
     for (const [opp, user] of (strategy.split(/(\w{2})/g).filter(str => str !== '').map(values => values.split(/(\w)/g).filter(str => str !== '')) as unknown) as `${keyof typeof rps}`[][]) overall_score.push(rps[user] + rps[opp] === rps[user] ? 3 : (rps[opp] === 2 || rps[user] === 2 ? (rps[opp] > rps[user] ? 0 : 6) : (rps[opp] > rps[user] ? 6 : 0)));
-    return overall_score.reduce((acc, value) => acc + value);
+    return overall_score.reduce((acc, value) => acc + value); // Add the values (numbers) in array to get the overall total score
 };
 
-// Day1Pt1(input)
+// console.log(Day1Pt1(input))
